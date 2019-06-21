1412bbc418ff5d718213d902f810847e017957d7
Aren Davey <adavey@andrew.cmu.edu>
2019-06-20 19:51:01 -0700

    hex grid working

--- js/background.js
+++ js/background.js
@@ -1,32 +1,54 @@
 var canvas;
+var mainLayout;
+var hexes = [];
+var center;
+var size;
+var origin;
 
 function windowResized()
 {
   resizeCanvas(windowWidth,windowHeight);
+  origin = Point(windowWidth/2,windowHeight/2);
+  resizeLayout(mainLayout, size, origin);
 }
+//generate the board once. its a hegagonal board
+function generateBoard(height, width)
+{
+  for (var r = 0; r < height; r++)
+  {
+    var rOff = Math.floor((r+1)/2);
+    for (var q = -rOff; q < width - rOff; q++)
+    {
+      hexes.push(Hex(q,r,-q-r));
+      hexes.push(Hex(-q,-r, q+r));
+    }
+  }
+}
+
+
 function setup()
 {
+  //canvas = createCanvas(400,400);
   canvas = createCanvas(windowWidth,windowHeight);
   canvas.position(0,0);
   canvas.style('z-index', '-1');
+  size = Point(20, 20);
+  origin = Point(windowWidth/2,windowHeight/2);
+  mainLayout = Layout(pointyOrient,size,origin);
+  generateBoard(11,11);
+  center = Hex(0,0,0);
+
 }
 
 function draw()
 {
-  background(255);
-  ellipse(160,160,30,30);
-}
-
-//creates a hexagon with the center x, y and a radius of size
-//pointy size up
-class Hex
-{
-  constructor(q,r,s)
+  background(100);
+  fill(255);
+  for (var i = 0; i < hexes.length; i++)
   {
-    this.x = x;
-    this.y = y;
-    this.s = s;
+    drawHex(mainLayout, hexes[i], 255);
   }
-
+  drawHex(mainLayout, center, '#e75f8d');
+  drawHex(mainLayout, Hex(2,5,-7), '#e75f8d');
 
 }
--- js/hexgrid.js
+++ js/hexgrid.js
@@ -1,5 +1,10 @@
+//*** to create ****//
+/*
+  define the point size,
+*/
 
 //a point on the coord
+//prolly dont need, i can use p5js
 function Point (x, y)
 {
   return{x: x, y: y};
@@ -43,4 +48,83 @@ function hexDir(dir)
 function getHexNeighbor(a, dir)
 {
   return hexAdd(a, hexDirs[dir]);
+}
+
+//start of orentation
+//f is the forward matrix. b is inverse.
+function Orientation(f0, f1, f2, f3, b0, b1, b2, b3, start_angle)
+{
+  return {f0: f0, f1: f1, f2: f2, f3: f3, b0: b0, b1: b1, b2: b2, b3: b3, start_angle: start_angle};
+}
+
+//im only using pointy so might as well only put that in.
+var pointyOrient = Orientation(Math.sqrt(3.0), Math.sqrt(3.0)/2.0, 0.0, 3.0/2.0, Math.sqrt(3.0)/3.0, -1.0/3.0, 0.0, 2.0/3.0, 0.5);
+
+//to convert between the grid and the screen
+function Layout(orientaion, size, origin) //orientaion, point, point
+{
+  return{orientaion: orientaion, size: size, origin: origin};
+}
+
+//ensures that the center will stay in the center when the window size is changed.
+function resizeLayout(layout, nSize, nOrigin)
+{
+  layout.size = nSize;
+  layout.origin = nOrigin;
+}
+
+//connecting p5js with this lib. use f matrix
+function hex2Screen(layout, hex) //layout, hex
+{
+  var ori = layout.orientaion;
+  var size = layout.size;
+  var o = layout.origin;
+  var x = (ori.f0 * hex.q + ori.f1*hex.r) * size.x;
+  var y = (ori.f2 * hex.q + ori.f3*hex.r) * size.y;
+  return Point(x + o.x, y + o.y);
+}
+//use b matrix
+function screen2Hex(layout, pixel) //layout, point
+{
+  var ori = layout.orientation;
+  var size = layout.size;
+  var o = layout.origin;
+  var pt = Point((p.x - o.x)/size.x, (p.y - o.y)/size.y);
+  var q = ori.b0 * pt.x + ori.b1 *pt.y;
+  var r = ori.b2 * pt.x + ori.b3 * pt.y;
+  return Hex(q, r, -q -r);
+}
+
+//creating the points for the corners of the hexagon
+function hexCorner(layout, corner) //layout, int
+{
+  var size = layout.size;
+  var ori = layout.orientaion;
+  var angle = 2.0 * Math.PI * (ori.start_angle + corner) / 6;
+  return Point(size.x * Math.cos(angle), size.y * Math.sin(angle));
+}
+function getCorners(layout, hex) //where they are on the screen.
+{
+  var corners= [];
+  var center = hex2Screen(layout, hex);
+  for (var i = 0; i < 6; i++)
+  {
+    var corner = hexCorner(layout, i);
+    corners.push(Point(center.x + corner.x, center.y + corner.y));
+  }
+  return corners;
+}
+
+//draw the hex from corners, uses p5
+function drawHex(layout, hex, color) //layout, hex
+{
+  var corners = getCorners(layout, hex);
+  fill(color);
+  beginShape();
+  for (var i = 0; i < 6; i++)
+  {
+    var pt = corners[i];
+    vertex(pt.x, pt.y);
+  }
+  endShape(CLOSE);
 }
